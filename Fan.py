33fe88cbfa5ec5b28106153212dca3c9ec5c1b08
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-07-03 16:03:41 +0800

    Defining all of the objects anf functions of the Fan

--- Fan.py
+++ Fan.py
@@ -4,3 +4,35 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    # Defining all of the objects anf functions of the Fan
+    def __init__(self, speed=1, radius=5, color='blue', on=False):
+        self._speed = speed
+        self._on = on
+        self._radius = radius
+        self._color = color
+
+    # Getters and setters
+    def get_speed(self):
+        return self._speed
+
+    def set_speed(self, speed):
+        self._speed = speed
+
+    def is_on(self):
+        return self._on
+
+    def set_on(self, on):
+        self._on = on
+
+    def get_radius(self):
+        return self._radius
+
+    def set_radius(self, radius):
+        self._radius = radius
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self, color):
+        self._color = color
